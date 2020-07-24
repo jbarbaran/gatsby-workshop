37fcd211f2abbb9485d0ed3956f462f6fbf85011
Javier Barbaran <javier.barbaran@gmail.com>
2020-07-24 21:16:45 +0200

    Slight modifications to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
+    siteMetadata: {
+        title: `Javi`,
+        author: `Javi`,
+        description: `Mi blog`,
+        siteUrl: `https://gatsby-workshop-site.netlify.app`
+    },
     plugins: [
-        `gatsby-plugin-mdx`
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-theme-blog`, 
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured: true
+            }
+        }
     ]
 }
